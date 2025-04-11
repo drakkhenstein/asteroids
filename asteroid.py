7a10c29549804d2f373045908d97d4e6551f05e2
drakkhenstein <greenknight84@hotmail.com>
2025-04-11 14:44:16 -0400

    Made asteroid field

---
+++ asteroid.py
@@ -0,0 +1,20 @@
+import pygame
+from circleshape import *
+from constants import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        #self.rotation = 0
+        #self.rotation_speed = 0
+        #self.rotation_speed = random.uniform(-ASTEROID_ROTATION_SPEED, ASTEROID_ROTATION_SPEED)
+        
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        #self.rotation += self.rotation_speed * dt
+        self.position += self.velocity * dt
+        #self.position.x %= SCREEN_WIDTH
+        #self.position.y %= SCREEN_HEIGHT
+        
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 from constants import *
 from player import *
 from circleshape import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
@@ -15,6 +17,12 @@ def main():
     #player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -23,11 +31,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
         updatable.update(dt)
         #group.update(dt)
+        
         for entity in drawable:
             entity.draw(screen)
         #player.draw(screen)
+        
         pygame.display.flip()
         
 
